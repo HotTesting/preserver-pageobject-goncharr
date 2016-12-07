58bbe20c0bd0f6b0fc3c16f1cab2eed03cedaafa
goncharr <alegoncharuk@gmail.com>
2016-12-07 16:48:05 +0200

    add jasmine reporter

--- protractor.conf.js
+++ protractor.conf.js
@@ -5,6 +5,31 @@ module.exports.config = {
     directConnect: true,
     baseUrl: 'http://www.hiteshbalar.com/preserver/notes',
     onPrepare: function () {
+
+      //jasmine.getEnv().addReporter({})
+
+        // Config for https://www.npmjs.com/package/jasmine-spec-reporter
+        // let SpecReporter = require('jasmine-spec-reporter')
+        // jasmine.getEnv().addReporter(new SpecReporter({
+        //     displayStacktrace: 'specs',     // display stacktrace for each failed assertion, values: (all|specs|summary|none)
+        //     displaySuccessesSummary: false, // display summary of all successes after execution
+        //     displayFailuresSummary: false,  // display summary of all failures after execution
+        //     displayPendingSummary: true,    // display summary of all pending specs after execution
+        //     displaySuccessfulSpec: true,    // display each successful spec
+        //     displayFailedSpec: true,        // display each failed spec
+        //     displayPendingSpec: true,       // display each pending spec
+        //     displaySpecDuration: true       // display each spec duration
+        // }))
+
+        // Config for https://www.npmjs.com/package/jasmine-console-reporter
+        let JasmineConsoleReporter = require('jasmine-console-reporter')
+        jasmine.getEnv().addReporter(new JasmineConsoleReporter({
+            colors: 1,           // (0|false)|(1|true)|2
+            cleanStack: 2,       // (0|false)|(1|true)|2|3
+            verbosity: 4,        // (0|false)|1|2|(3|true)|4
+            listStyle: 'indent', // "flat"|"indent"
+            activity: false      // If your tests log extra data to console, this option should be disabled
+}))
       
       beforeEach(function () {
       browser.get('')
